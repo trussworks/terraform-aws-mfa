4dd3005463ae3a873d68a666848092d3b83c063b
kilbergr <kilbergr@users.noreply.github.com>
2019-11-06 13:47:58 -0800

    add default region

--- test/terraform_aws_mfa_test.go
+++ test/terraform_aws_mfa_test.go
@@ -3,14 +3,20 @@ package test
 import (
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
 func TestTerraformAwsMfa(t *testing.T) {
 	t.Parallel()
 
+	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../examples/simple/",
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": awsRegion,
+		},
 	}
 
 	defer terraform.Destroy(t, terraformOptions)
